813ea8aaea7121c998eb4ebedecaa9755e1759fe
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2025-02-09 16:58:47 -0600

    My first commit, created my first component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Greeting from './Components/Greeting'
+
+function App() {
+  return (
+    <div>
+      <Greeting />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Greeting.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Greeting() {
+  return (
+    <div>
+      <h1>Welcome to My First React App</h1>
+      <p>This is my first component!</p>
+    </div>
+  )
+}
+
+export default Greeting
